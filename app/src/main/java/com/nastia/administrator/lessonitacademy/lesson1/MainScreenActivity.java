331275e8a482f17ec7cc6036caa1d5d48ab996fd
Anastasia <krasnova.an.23@gmail.com>
2018-09-19 10:40:34 +0300

    add MainScreenActivity.java add activity_main_screen.xml

---
+++ app/src/main/java/com/nastia/administrator/lessonitacademy/lesson1/MainScreenActivity.java
@@ -0,0 +1,43 @@
+package com.nastia.administrator.lessonitacademy.lesson1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.nastia.administrator.lessonitacademy.R;
+
+public class MainScreenActivity extends Activity {
+
+    Button btnHw1;
+    Button btnHw2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+        btnHw1 = findViewById(R.id.btn_hw1);
+        btnHw2 = findViewById(R.id.btn_hw2);
+
+        btnHw1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainScreenActivity.this, Lesson1Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnHw2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast toast = Toast.makeText(getApplicationContext(), "In process...", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+}
