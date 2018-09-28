86082ec0d0c9308f1a0c41812fdbf1d52d282168
Nastia <--remove-section>
2018-09-28 14:01:17 +0300

    commit 28.09

---
+++ app/src/main/java/lesson4/Lesson4Activity.java
@@ -0,0 +1,15 @@
+package lesson4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.nastia.administrator.lessonitacademy.R;
+
+public class Lesson4Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson4);
+    }
+}
