4625303f64b5b708c7880b93c9198778be89ac5b
Anastasia <krasnova.an.23@gmail.com>
2018-09-30 13:31:54 +0300

    add animation begin adding circle

---
+++ app/src/main/java/lesson4/CircleView.java
@@ -0,0 +1,66 @@
+package lesson4;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CircleView extends View {
+
+    public int circleColor = Color.BLUE;
+    private Paint paint;
+
+    public CircleView(Context context) {
+        super(context);
+        init();
+
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(circleColor);
+    }
+
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int w = getWidth();
+        int h = getHeight();
+
+        int pl = getPaddingLeft();
+        int pr = getPaddingRight();
+        int pt = getPaddingTop();
+        int pb = getPaddingBottom();
+
+        int usableWidth = w - (pl + pr);
+        int usableHeight = h - (pt + pb);
+
+        float radius = Math.min(usableWidth, usableHeight) / 2;
+        float cx = pl + (usableWidth / 2);
+        float cy = pt + (usableHeight / 2);
+
+        canvas.drawCircle(cx, cy, radius, paint);
+    }
+}
--- app/src/main/java/lesson4/Lesson4Activity.java
+++ app/src/main/java/lesson4/Lesson4Activity.java
@@ -1,15 +1,27 @@
 package lesson4;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.nastia.administrator.lessonitacademy.R;
+import static com.nastia.administrator.lessonitacademy.R.drawable.owl_anim;
 
 public class Lesson4Activity extends Activity {
+
+    ImageView iv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson4);
+
+        iv1 = findViewById(R.id.image_view);
+
+        iv1.setBackgroundResource(R.drawable.owl_anim);
+        AnimationDrawable animation = (AnimationDrawable)iv1.getBackground();
+
+        animation.start();
     }
 }
