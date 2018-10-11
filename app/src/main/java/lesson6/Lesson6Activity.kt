56ee139cfddad7a508b83285af716a56ceecbc5e
Anastasia <krasnova.an.23@gmail.com>
2018-10-11 23:10:48 +0300

    add lesson6

--- app/src/main/java/lesson6/Lesson6Activity.kt
+++ app/src/main/java/lesson6/Lesson6Activity.kt
@@ -1,78 +1,76 @@
 package lesson6
 
-import android.annotation.TargetApi
 import android.content.*
 import android.net.ConnectivityManager
-import android.net.wifi.WifiManager
-import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
-import android.os.PersistableBundle
-import android.support.annotation.RequiresApi
-import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 import com.nastia.administrator.lessonitacademy.R
-
+import android.content.Intent
+import android.widget.Toast
+import android.content.BroadcastReceiver
+import android.support.v4.content.LocalBroadcastManager
 
 
 class Lesson6Activity : AppCompatActivity() {
 
     lateinit var changeWifi: TextView
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lesson6)
-    }
 
-    var receiver: BroadcastReceiver = object : BroadcastReceiver() {
+        val intent = Intent(this, MyService::class.java)
+        startService(intent)
 
-        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-        override fun onReceive(context: Context, intent: Intent) {
+        changeWifi = findViewById(R.id.wifi_switcher)
+        changeWifi.setOnClickListener {
 
             val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             val mWifi = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
 
-            if (mWifi.isConnected) {
-                wifiManager(true)
-                changeWifi.background = getDrawable(R.color.green)
-            } else {
-                wifiManager(false)
-                changeWifi.background = getDrawable(R.color.red)
-            }
+            serviceConnector.setWifi(!mWifi.isConnected)
         }
     }
 
-    override fun onStart() {
-        super.onStart()
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val status = intent.getBooleanExtra("wifiStatus", false)
+            var mess = "off"
+            if (status){
+                mess="on"
+            }
+            //get the type of message from MyGcmListenerService 1 - lock or 0 -Unlock
+            Toast.makeText(application, mess, Toast.LENGTH_LONG).show()
+        }
     }
+
     override fun onResume() {
         super.onResume()
-
         bindService(Intent(this, MyService::class.java),
                 serviceConnector, Context.BIND_AUTO_CREATE)
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, IntentFilter("NOW"))
     }
 
     override fun onPause() {
         super.onPause()
         unbindService(serviceConnector)
-        unregisterReceiver(receiver)
+        unregisterReceiver(broadcastReceiver)
     }
 
     private val serviceConnector = object : ServiceConnection {
         override fun onServiceDisconnected(name: ComponentName?) {
         }
 
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-        }
-    }
+        private lateinit var localBinder: MyService.LocalBinder
 
-    fun wifiManager(status: Boolean){
-        val wifiManager = this.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        if (status && !wifiManager.isWifiEnabled) {
-            wifiManager.isWifiEnabled = true
-        } else if (!status && wifiManager.isWifiEnabled) {
-            wifiManager.isWifiEnabled = false
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            localBinder = service as MyService.LocalBinder
+        }
+        fun setWifi(state:Boolean){
+            localBinder.service.wifiManager(state)
         }
     }
 }
--- app/src/main/java/lesson6/MyService.kt
+++ app/src/main/java/lesson6/MyService.kt
@@ -1,20 +1,31 @@
 package lesson6
 
 import android.app.Service
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.wifi.WifiManager
+import android.os.Binder
 import android.os.IBinder
+import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
 import android.widget.Toast
 
 class MyService : Service() {
 
+    private val mBinder = LocalBinder()
     override fun onCreate() {
         super.onCreate()
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION)
+        registerReceiver(receiver, intentFilter)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -25,10 +36,35 @@ class MyService : Service() {
 
     override fun onBind(intent: Intent?): IBinder? {
         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
-        return null
+        return mBinder
     }
 
     override fun onUnbind(intent: Intent?): Boolean {
         return super.onUnbind(intent)
     }
+
+    inner class LocalBinder : Binder() {
+        val service: MyService
+            get() = this@MyService
+    }
+
+    fun wifiManager(status: Boolean){
+
+        val wifi = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        wifi.isWifiEnabled = status
+    }
+
+    internal var receiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+
+            val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val mWifi = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+
+            val intent = Intent()
+            intent.putExtra("wifiStatus", mWifi.isConnected)
+            intent.action = "NOW"
+            LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(intent)
+        }
+    }
 }
+
