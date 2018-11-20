27a3eeffbae0da969bb98bb528b035817e1e0e3d
Anastasia <krasnova.an.23@gmail.com>
2018-11-20 17:59:20 +0300

    add lesson 16

---
+++ app/src/main/java/lesson16/Lesson16Activity.kt
@@ -0,0 +1,13 @@
+package lesson16
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.nastia.administrator.lessonitacademy.R
+
+class Lesson16Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lesson16)
+    }
+}
